5abae709dfc818138742ea8b82e36495dd93dd54
Eric White <led.ecodyswhite@gmail.com>
2022-12-13 07:42:48 -0800

    wake me up inside

--- music.sql
+++ music.sql
@@ -7,27 +7,179 @@ CREATE DATABASE music;
 
 \c music
 
+-- CREATE TABLE songs
+-- (
+--   id SERIAL PRIMARY KEY,
+--   title TEXT NOT NULL,
+--   duration_in_seconds INTEGER NOT NULL,
+--   release_date DATE NOT NULL,
+--   artists TEXT[] NOT NULL,
+--   album TEXT NOT NULL,
+--   producers TEXT[] NOT NULL
+-- );
+
+-- INSERT INTO songs
+--   (title, duration_in_seconds, release_date, artists, album, producers)
+-- VALUES
+--   ('MMMBop', 238, '04-15-1997', '{"Hanson"}', 'Middle of Nowhere', '{"Dust Brothers", "Stephen Lironi"}'),
+--   ('Bohemian Rhapsody', 355, '10-31-1975', '{"Queen"}', 'A Night at the Opera', '{"Roy Thomas Baker"}'),
+--   ('One Sweet Day', 282, '11-14-1995', '{"Mariah Cary", "Boyz II Men"}', 'Daydream', '{"Walter Afanasieff"}'),
+--   ('Shallow', 216, '09-27-2018', '{"Lady Gaga", "Bradley Cooper"}', 'A Star Is Born', '{"Benjamin Rice"}'),
+--   ('How You Remind Me', 223, '08-21-2001', '{"Nickelback"}', 'Silver Side Up', '{"Rick Parashar"}'),
+--   ('New York State of Mind', 276, '10-20-2009', '{"Jay Z", "Alicia Keys"}', 'The Blueprint 3', '{"Al Shux"}'),
+--   ('Dark Horse', 215, '12-17-2013', '{"Katy Perry", "Juicy J"}', 'Prism', '{"Max Martin", "Cirkut"}'),
+--   ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
+--   ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
+--   ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+
+
+--makes your music database better
+
+CREATE TABLE producers
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE artists
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE albums
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  release_date DATE NOT NULL
+);
+
 CREATE TABLE songs
 (
   id SERIAL PRIMARY KEY,
   title TEXT NOT NULL,
   duration_in_seconds INTEGER NOT NULL,
-  release_date DATE NOT NULL,
-  artists TEXT[] NOT NULL,
-  album TEXT NOT NULL,
-  producers TEXT[] NOT NULL
+  albumID INTEGER REFERENCES albums
 );
 
-INSERT INTO songs
-  (title, duration_in_seconds, release_date, artists, album, producers)
+CREATE TABLE producers_albums
+(
+  id SERIAL PRIMARY KEY,
+  producerID INTEGER REFERENCES producers,
+  albumID INTEGER REFERENCES albums,
+  songID INTEGER REFERENCES songs
+);
+
+CREATE TABLE artists_albums
+(
+  id SERIAL PRIMARY KEY,
+  artistID INTEGER REFERENCES artists,
+  albumID INTEGER REFERENCES albums,
+  songID INTEGER REFERENCES songs
+);
+
+
+INSERT INTO producers
+  (name)
 VALUES
-  ('MMMBop', 238, '04-15-1997', '{"Hanson"}', 'Middle of Nowhere', '{"Dust Brothers", "Stephen Lironi"}'),
-  ('Bohemian Rhapsody', 355, '10-31-1975', '{"Queen"}', 'A Night at the Opera', '{"Roy Thomas Baker"}'),
-  ('One Sweet Day', 282, '11-14-1995', '{"Mariah Cary", "Boyz II Men"}', 'Daydream', '{"Walter Afanasieff"}'),
-  ('Shallow', 216, '09-27-2018', '{"Lady Gaga", "Bradley Cooper"}', 'A Star Is Born', '{"Benjamin Rice"}'),
-  ('How You Remind Me', 223, '08-21-2001', '{"Nickelback"}', 'Silver Side Up', '{"Rick Parashar"}'),
-  ('New York State of Mind', 276, '10-20-2009', '{"Jay Z", "Alicia Keys"}', 'The Blueprint 3', '{"Al Shux"}'),
-  ('Dark Horse', 215, '12-17-2013', '{"Katy Perry", "Juicy J"}', 'Prism', '{"Max Martin", "Cirkut"}'),
-  ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
-  ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
-  ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+  ('Dust Brothers'),
+  ('Stephen Lironi'),
+  ('Roy Thomas Baker'),
+  ('Walter Afanasieff'),
+  ('Benjamin Rice'),
+  ('Rick Parashar'),
+  ('Al Shux'),
+  ('Max Martin'),
+  ('Cirkut'),
+  ('Shellback'),
+  ('Benny Blanco'),
+  ('The Matrix'),
+  ('Darkchild');
+
+
+INSERT INTO artists
+  (name)
+VALUES
+  ('Hanson'),
+  ('Queen'),
+  ('Mariah Carey'),
+  ('Boyz II Men'),
+  ('Lady Gaga'),
+  ('Bradley Cooper'),
+  ('Nickelback'),
+  ('Jay Z'),
+  ('Alicia Keys'),
+  ('Katy Perry'),
+  ('Juicy J'),
+  ('Maroon 5'),
+  ('Christina Aguilera'),
+  ('Avril Lavigne'),
+  ('Destiny''s Child');
+
+
+INSERT INTO albums
+  (name, release_date)
+VALUES
+  ('Middle of Nowhere', '04-15-1997'),
+  ('A Night at the Opera', '10-31-1975'),
+  ('Daydream', '11-14-1995'),
+  ('A Star Is Born', '09-27-2018'),
+  ('Silver Side Up', '08-21-2001'),
+  ('The Blueprint 3', '10-20-2009'),
+  ('Prism', '12-17-2013'),
+  ('Hands All Over', '06-21-2011'),
+  ('Let''s Go', '05-14-2002'),
+  ('The Writing''s on the Wall', '11-07-1999');
+
+
+INSERT INTO songs
+  (title, duration_in_seconds, albumID)
+VALUES
+  ('MMMBop', 238, 1),
+  ('Bohemian Rhapsody', 355, 2),
+  ('One Sweet Day', 282, 3),
+  ('Shallow', 216, 4),
+  ('How You Remind Me', 223, 5),
+  ('New York State of Mind', 276, 6),
+  ('Dark Horse', 215, 7),
+  ('Moves Like Jagger', 201, 8),
+  ('Complicated', 244, 9),
+  ('Say My Name', 240, 10);
+
+
+INSERT INTO producers_albums
+  (producerID, albumID, songID)
+VALUES
+  (1, 1, 1),
+  (2, 1, 1),
+  (3, 2, 2),
+  (4, 3, 3), 
+  (5, 4, 4),
+  (6, 5, 5),
+  (7, 6, 6),
+  (8, 7, 7),
+  (9, 7, 7),
+  (10, 8, 8),
+  (11, 8, 8),
+  (12, 9, 9),
+  (13, 10, 10);
+
+
+INSERT INTO artists_albums
+  (artistID, albumID, songID)
+VALUES
+  (1, 1, 1),
+  (2, 2, 2),
+  (3, 3, 3),
+  (4, 3, 3),
+  (5, 4, 4),
+  (6, 4, 4),
+  (7, 5, 5),
+  (8, 6, 6),
+  (9, 6, 6),
+  (10, 7, 7),
+  (11, 7, 7),
+  (12, 8, 8),
+  (13, 8, 8),
+  (14, 9, 9),
+  (15, 10, 10);
--- outer_space.sql
+++ outer_space.sql
@@ -7,24 +7,94 @@ CREATE DATABASE outer_space;
 
 \c outer_space
 
+-- CREATE TABLE planets
+-- (
+--   id SERIAL PRIMARY KEY,
+--   name TEXT NOT NULL,
+--   orbital_period_in_years FLOAT NOT NULL,
+--   orbits_around TEXT NOT NULL,
+--   galaxy TEXT NOT NULL,
+--   moons TEXT[]
+-- );
+
+-- INSERT INTO planets
+--   (name, orbital_period_in_years, orbits_around, galaxy, moons)
+-- VALUES
+--   ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
+--   ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
+--   ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
+--   ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
+--   ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
+--   ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+
+-- so what we're looking for is a separation of concerns
+
+CREATE TABLE milky_way
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE the_sun
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  galaxyID INTEGER REFERENCES milky_way 
+);
+
 CREATE TABLE planets
 (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   orbital_period_in_years FLOAT NOT NULL,
-  orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  orbits_around INTEGER REFERENCES the_sun,
+  galaxyID INTEGER REFERENCES milky_way
 );
 
-INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
-VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+CREATE TABLE moons
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  planetID INTEGER REFERENCES planets
+);
 
--- so what we're looking for is a separation of concerns
+INSERT INTO milky_way
+(name)
+VALUES 
+('Milky Way');
+
+INSERT INTO the_sun
+(name, galaxyID)
+VALUES
+('The Sun', 1), ('Proxima Centauri', 1), ('Gliese 876', 1);
+
+INSERT INTO planets
+  (name, orbital_period_in_years, orbits_around, galaxyID)
+VALUES
+  ('Earth', 1.00, 1, 1),
+  ('Mars', 1.88, 1, 1),
+  ('Venus', 0.62, 1, 1),
+  ('Neptune', 164.8, 1, 1),
+  ('Proxima Centauri b', 0.03, 2, 1),
+  ('Gliese 876 b', 0.23, 3, 1);
+
+  INSERT INTO moons
+    (name, planetID)
+  VALUES
+    ('The Moon', 1), 
+    ('Phobos', 2), 
+    ('Deimos', 2), 
+    ('Naiad', 4), 
+    ('Thalassa', 4), 
+    ('Despina', 4), 
+    ('Galatea', 4), 
+    ('Larissa', 4), 
+    ('S/2004 N 1', 4), 
+    ('Proteus', 4), 
+    ('Triton', 4), 
+    ('Nereid', 4), 
+    ('Halimede', 4), 
+    ('Sao', 4), 
+    ('Laomedeia', 4), 
+    ('Psamanthe', 4), 
+    ('Neso', 4);
